a764c413ec83dfaf6020e7edd419a4d185aae9aa
SamuraIkenz-code <ikenz2023@gmail.com>
2025-08-22 02:17:30 -1100

    My first project

---
+++ Introduction to JavaScript/script.js
@@ -0,0 +1,93 @@
+// variables
+// how to declare a variable
+
+let studentName="Zara";
+console.log(studentName)
+
+// how to update a variable
+
+studentName="Aisha"
+
+// exercise
+
+let myHobby="Reading";
+console.log(myHobby)
+
+myHobby="Movies";
+console.log(myHobby)
+
+// Number
+
+let number= 2;
+
+// string
+
+let letter="3";
+
+// Boolean
+
+let isHappy= true;
+
+// undefined
+
+
+// null
+
+
+
+// array
+
+let subjects=["english","math","science","businessStudies","geography","history","religion","socialStudies","PHE"];
+console.log(subjects)
+
+// how to access the first item on an array list
+
+console.log(subjects[subjects.length-1]);
+
+// how to get the total number of items
+
+console.log(subjects.length)
+
+// how to update an item in an array
+
+subjects[1]="homeEconomics";
+console.log(subjects)
+
+// object basics
+
+let student ={
+    name:"emma",
+    school:"codeville",
+    age:15
+
+}
+
+console.log( student.name );
+
+student.age=10 
+student.hobby="swimming"
+
+console.log(student)
+
+let bestMovie={
+
+    name:"Mission Impossible",
+    releaseDate:"1996",
+    starActor:"Tom Cruise",
+    genre:"Action"
+}
+
+console.log (bestMovie)
+
+// conditionals
+
+let isSunny=false;
+if(isSunny){
+    console.log("go out freely");
+} 
+ 
+else{
+
+    console.log("take an umbrella");
+}
+
